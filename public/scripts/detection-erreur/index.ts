f8a334e5517aa49fb5f2dd93273c8c5ff873afae
Marissa belle <marissabellebindeki@gmail.com>
2025-10-21 13:21:55 -0400

    Refactor index.ts: export User type, update values array, and improve greet function formatting

--- public/scripts/detection-erreur/index.ts
+++ public/scripts/detection-erreur/index.ts
@@ -1,19 +1,18 @@
-type User = {
+export type User = {
     username: string;
 }
 
-const values = [1, 2, 3, 4, 5];
+const values = [1, 2, 3, 4, 5, 6];
 
-if (values.length > 2) {
-    console.log("array has more than two items");
-}else {
-    console.log("array has two items or less");
+if(values.length > 2){
+    console.log("Array has more than two items");
+} else{
+    console.log("Array has two items or less");
 }
-
 
 function greet(user: User){
-    return "Hello, " + user.username.toUpperCase();
+    return "Hello "+user.username.toUpperCase();
 }
 
-const user: User = { username: "Marissabelle" };
+const user: User = {username: "Yann"};
 greet(user);
