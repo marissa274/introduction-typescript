7d2ca914ce323206ba5ff9413817d73836ad80c9
Marissa belle <marissabellebindeki@gmail.com>
2025-10-21 13:15:13 -0400

    Replace message logging with count, add, and printValue function implementations

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
-const message = "World";
-console.log("Hello ads asd asd ", message);
+"use strict";
+let count = 5;
+count = 10;
+function add(a, b) {
+    return a + b;
+}
+const result = add(4, 5);
+//type en js:  boolean, String, number, undefined, symbol, fonction, objet, bigint
+//use let x: unknown    if you don't know the type of the value to be parsed
+//use the or operator to assign either of two types example    x: string | number
+function printValue(x) {
+    if (typeof x === "string") {
+        console.log(x.toUpperCase());
+    }
+    else {
+        console.log(x.toFixed());
+    }
+}
